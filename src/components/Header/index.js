eec021768e7e9d5bdaa94f65494c4332b0469c2b
Igor Souza <igorsouza.dev@gmail.com>
2019-09-03 20:26:35 -0300

    Adding the home layout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,18 +7,18 @@ import { Container, Cart } from './styles';
 import logo from '../../assets/images/logo.svg';
 
 export default function Header() {
-    return (
-        <Container>
-            <Link to="/">
-                <img src={logo} alt="Rocketshoes" />
-            </Link>
-            <Cart to="/cart">
-                <div>
-                    <strong>My Cart</strong>
-                    <span>3 items</span>
-                </div>
-                <MdShoppingBasket size={36} color="#fff" />
-            </Cart>
-        </Container>
-    );
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes" />
+      </Link>
+      <Cart to="/cart">
+        <div>
+          <strong>My Cart</strong>
+          <span>3 items</span>
+        </div>
+        <MdShoppingBasket size={36} color="#fff" />
+      </Cart>
+    </Container>
+  );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,109 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-    return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://avatars0.githubusercontent.com/u/54104600?s=460&v=4"
+          alt="thumbnail"
+        />
+        <strong>Um produto</strong>
+        <span>R$129,20</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart color="#fff" size={16} /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://avatars0.githubusercontent.com/u/54104600?s=460&v=4"
+          alt="thumbnail"
+        />
+        <strong>Um produto</strong>
+        <span>R$129,20</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart color="#fff" size={16} /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://avatars0.githubusercontent.com/u/54104600?s=460&v=4"
+          alt="thumbnail"
+        />
+        <strong>Um produto</strong>
+        <span>R$129,20</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart color="#fff" size={16} /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://avatars0.githubusercontent.com/u/54104600?s=460&v=4"
+          alt="thumbnail"
+        />
+        <strong>Um produto</strong>
+        <span>R$129,20</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart color="#fff" size={16} /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://avatars0.githubusercontent.com/u/54104600?s=460&v=4"
+          alt="thumbnail"
+        />
+        <strong>Um produto</strong>
+        <span>R$129,20</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart color="#fff" size={16} /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://avatars0.githubusercontent.com/u/54104600?s=460&v=4"
+          alt="thumbnail"
+        />
+        <strong>Um produto</strong>
+        <span>R$129,20</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart color="#fff" size={16} /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://avatars0.githubusercontent.com/u/54104600?s=460&v=4"
+          alt="thumbnail"
+        />
+        <strong>Um produto</strong>
+        <span>R$129,20</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart color="#fff" size={16} /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
